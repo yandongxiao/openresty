861b98fa543df68f13ffb7380a23b643d7474cd8
yandongxiao <dxyan06@gmail.com>
2018-01-06 17:50:57 +0800

    多重赋值
    在Python中，多重赋值要求变量数和值的数量是完全相等的
    在Lua中，因为没有区分变量定义与变量赋值, 按照这个思路，Lua允许变量数和值
    的数量不相等

---
+++ lua/variable/d_multi_assignment.lua
@@ -0,0 +1,36 @@
+--[[
+--
+-- 变量的赋值，也是变量的定义
+-- 分为三种情况
+--  变量个数==值得个数
+--  变量个数 < 值个数
+--  变量个数 > 值个数   注意无论多余变量是否已经定义，变量的值都将为nil
+--
+--  python:
+--    python也支持多重赋值，但是python要求变量个数与值的个数必须是完全相等
+--    否则，出错
+--]]
+
+-- 1. 等于
+a, b, c = 1, 2, 3
+assert(a==1 and b==2 and c==3)
+
+-- 2. 大于, 且cc未定义
+aa, bb, cc = 1, 2
+assert(aa==1 and bb==2 and cc==nil)
+
+-- 3. 大于, 且cc定义
+cc=10
+aa, bb, cc = 1, 2
+assert(aa==1 and bb==2 and cc==nil)
+
+-- 小于
+xx, yy = 1, 2, 3
+assert(xx==1 and yy==2)
+
+-- 值之间的交换
+a = 10
+b = 20
+a, b = b, a
+assert(a == 20)
+assert(b == 10)
