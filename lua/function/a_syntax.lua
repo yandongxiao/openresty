75c85fbfe6ef3f82c450329ec1f85575322ee2ae
yandongxiao <dxyan06@gmail.com>
2018-01-07 17:19:22 +0800

    function
    function是定义函数的关键字
    function myfunc()
    end

---
+++ lua/function/a_syntax.lua
@@ -0,0 +1,40 @@
+--[[
+-- 函数定义：由关键字function，函数名称，参数值和block分隔符组成
+-- 函数定义、函数调用、函数返回都采用主流的函数模型
+--]]
+
+function myfunc(num1, num2)
+end
+
+-- 1. 实参和形参的个数不一定相等
+-- 跟LUA支持的多重赋值有关系
+myfunc(1,2,3,4)
+myfunc()
+
+-- 2. LUA特殊方式调用
+-- 一个函数若只有一个参数，并且此参数是常量的字符串或者表，那么圆括号便可以省略掉
+-- NOTICE: 必须是常量
+-- string
+print "Hello World"
+print [[a multi-line
+          message]]
+
+-- table
+function myfunc(t)
+    print(t.name)
+end
+
+myfunc {
+    name = "jack"
+}
+
+-- 3. 利用unpack函数传递参数
+-- 参见：i_variable_agrs.lua
+
+-- 4. NOTE: 函数参数定义了新的局部变量
+num1 = 10
+function myfunc(num1, num2)
+    print(num1)
+end
+myfunc(20)
+print(num1)
