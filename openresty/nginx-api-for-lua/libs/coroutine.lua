b6892b3d776834fb48d4ccb9f75628653d157ca2
yandongxiao <dxyan06@gmail.com>
2018-01-27 21:35:41 +0800

    coroutine.create
    父子协程之间的工作方式与标准的父子协程的工作方式完全相同。
    必须调用coroutine.yield才会主动"释放"CPU

--- openresty/nginx-api-for-lua/libs/coroutine.lua
+++ openresty/nginx-api-for-lua/libs/coroutine.lua
@@ -1,5 +1,6 @@
 function sum(a, b)
-    ngx.sleep(1)
+    ngx.say(ngx.now())
+    ngx.sleep(10)
     ngx.say("sum...")
     return a+b
 end
@@ -8,8 +9,9 @@ end
 thread = coroutine.create(sum)
 assert(type(thread) == "thread")
 
--- 运行子协程，子协程虽然调用了ngx.sleep但是貌似它并不会释放计算资源
+-- 运行子协程，子协程虽然调用了ngx.sleep但是"貌似"它并不会释放计算资源
 -- 子协程必须调用coroutine.yield来主动释放CPU
+-- NOTE: ngx.sleep被调用时，协程实际上释放了资源（因为并发发送curl请求时，并没有被阻塞)
 ngx.say("entry thread")
 status, rt = coroutine.resume(thread, 1, 2)
 assert(status==true)
